82a53de72f0b9ee1408dca3d9889aeecb539d385
obayedsiam <obayedsiam@gmail.com>
2022-06-08 18:36:56 +0600

    "reading from file"

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import openpyxl
 import selenium
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -17,46 +18,6 @@ allBookName = []  # Array of reviewed book name
 allBookWriterName = []  # Array of book writers' name
 allBookUrlLink = []  # Array of URL Link
 
-# Getting http page from the url
-driver.get("https://www.rokomari.com/book/226821/ebar-bhinno-kichu-hok?ref=fl2_p4")
-content = driver.page_source
-soup = BeautifulSoup(content, features="html.parser")
-
-# Finding all the lists
-reviewList = soup.find_all('p', attrs={'class': 'review-text js--review-content'})
-dateOfReviewList = soup.find_all('p', attrs={'class': 'date d-inline-block'})
-nameOfTheReviewer = soup.find_all('p', attrs={'class': 'd-inline-block name'})
-
-Book = soup.find('div', attrs={'class': 'details-book-main-info__header'})
-Writer = soup.find('p', attrs={'class': 'details-book-info__content-author'})
-
-# Book Name
-print(Book.text)
-
-# Writer Name
-print(Writer.find('a').contents[0])
-
-# Review from List
-for singleReview in reviewList:
-    print(singleReview.text)
-    allReviewList.append(singleReview.text)
-    allBookName.append(Book.text)
-    allBookWriterName.append(Writer.find('a').contents[0])
-
-# Review Date from List
-for singleDate in dateOfReviewList:
-    if len(singleDate.text) > 0:
-        print(singleDate.text)
-        allReviewDate.append(singleDate.text)
-
-# Reviewer Name from List
-for p in nameOfTheReviewer:
-    k = p.text.replace("By ", "")
-    k = k.replace(", ", "")
-    if len(k) > 0:
-        print(k)
-        allReviewerName.append(k)
-
 # Array for header
 a = {'Book Name': allBookName,
      'Writer Name': allBookWriterName,
@@ -64,11 +25,60 @@ a = {'Book Name': allBookName,
      'Review Date': allReviewDate,
      'Review': allReviewList}
 
-# Inserting into Excel File
-df = pd.DataFrame.from_dict(a, orient='index')
-df = df.transpose()
-# df.to_excel(r'C:\Users\EATL\PycharmProjects\WebScapping\products.xlsx', index=True, encoding='utf-8')
-df.to_excel(r'H:\Programming\pyCharm\WebScrappingPython\products.xlsx', index=True, encoding='utf-8')
+linkUrlFile = openpyxl.load_workbook('C:\\Users\\EATL\\PycharmProjects\\WebScapping\\link.xlsx', data_only=True)
+sheet_obj = linkUrlFile.active
+m_row = sheet_obj.max_row
+print(m_row)
+
+for i in range(1, 3):
+    cell_obj = sheet_obj.cell(row=i, column=2)
+    linkAddress = cell_obj.value
+    print(linkAddress)
+    # Getting http page from the url
+    driver.get(linkAddress)
+    content = driver.page_source
+    soup = BeautifulSoup(content, features="html.parser")
+
+    # Finding all the lists
+    reviewList = soup.find_all('p', attrs={'class': 'review-text js--review-content'})
+    dateOfReviewList = soup.find_all('p', attrs={'class': 'date d-inline-block'})
+    nameOfTheReviewer = soup.find_all('p', attrs={'class': 'd-inline-block name'})
+
+    Book = soup.find('div', attrs={'class': 'details-book-main-info__header'})
+    Writer = soup.find('p', attrs={'class': 'details-book-info__content-author'})
+
+    # Book Name
+    print(Book.text)
+
+    # Writer Name
+    print(Writer.find('a').contents[0])
+
+    # Review from List
+    for singleReview in reviewList:
+        print(singleReview.text)
+        allReviewList.append(singleReview.text)
+        allBookName.append(Book.text)
+        allBookWriterName.append(Writer.find('a').contents[0])
+
+    # Review Date from List
+    for singleDate in dateOfReviewList:
+        if len(singleDate.text) > 0:
+            print(singleDate.text)
+            allReviewDate.append(singleDate.text)
+
+    # Reviewer Name from List
+    for p in nameOfTheReviewer:
+        k = p.text.replace("By ", "")
+        k = k.replace(", ", "")
+        if len(k) > 0:
+            print(k)
+            allReviewerName.append(k)
+
+    # Inserting into Excel File
+    df = pd.DataFrame.from_dict(a, orient='index')
+    df = df.transpose()
+    df.to_excel(r'C:\Users\EATL\PycharmProjects\WebScapping\products.xlsx', index=True, encoding='utf-8')
+    # df.to_excel(r'H:\Programming\pyCharm\WebScrappingPython\products.xlsx', index=True, encoding='utf-8')
 
 #########################################################################################################
 # allBookName.append(nameOfTheBook)
