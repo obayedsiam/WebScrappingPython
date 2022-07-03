f4287ea79812e17d7bdb15ac7c89ffc6bbb41184
obayedsiam <obayedsiam@gmail.com>
2022-07-03 18:47:06 +0600

    "Up "

--- GetBookUrlFromWriter.py
+++ GetBookUrlFromWriter.py
@@ -7,24 +7,25 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 def getBookUrlFromWriterUrl(options, driver):
     # Collecting Books URL
     booksLinkList = []
     basicUrl = "https://www.rokomari.com"
     empltyBookUrl = 0
     c = {'Books Link': booksLinkList}
-    #linkUrlFile = openpyxl.load_workbook('H:\Programming\pyCharm\WebScrappingPython\AllWriter.xlsx', data_only=True)
-    linkUrlFile = openpyxl.load_workbook(r'C:\Users\EATL\PycharmProjects\WebScapping\AllWriter.xlsx', data_only=True)
+    # linkUrlFile = openpyxl.load_workbook('H:\Programming\pyCharm\WebScrappingPython\AllWriterValidUrl_Part_1.xlsx', data_only=True)
+    linkUrlFile = openpyxl.load_workbook(r'C:\Users\EATL\PycharmProjects\WebScapping\AllWriterValidUrl_Part_1.xlsx', data_only=True)
     sheet_obj = linkUrlFile.active
     m_row = sheet_obj.max_row
     print(m_row)
 
-    for i in range(184, 187):
+    for i in range(2, m_row):
         cell_obj = sheet_obj.cell(row=i, column=2)
         linkAddress = cell_obj.value
         isNextPresent = True
         while isNextPresent:
-            print("Writer Url Link : ", (i-2))
+            print("Writer Url Link : ", (i - 2))
             print(linkAddress)
             driver.get(linkAddress)
             content = driver.page_source
@@ -39,23 +40,23 @@ def getBookUrlFromWriterUrl(options, driver):
                     aTag = listTemp.find('a')
                     href = aTag.get('href')
                     print(href)
-                    booksLinkList.append(basicUrl+href)
+                    booksLinkList.append(basicUrl + href)
                     try:
-                       my_element = driver.find_element_by_xpath("//a[text()='next']")
-                       # print(my_element.get_attribute('href'))
-                       linkAddress = my_element.get_attribute('href')
+                        my_element = driver.find_element_by_xpath("//a[text()='next']")
+                        # print(my_element.get_attribute('href'))
+                        linkAddress = my_element.get_attribute('href')
                     except NoSuchElementException:
-                       isNextPresent = False
-                       print("End of Writer Book List")
-            else :
-                i = i+1
+                        isNextPresent = False
+                        print("End of Writer Book List")
+            else:
+                i = i + 1
                 cell_obj = sheet_obj.cell(row=i, column=2)
                 linkAddress = cell_obj.value
-                empltyBookUrl = empltyBookUrl+1
+                empltyBookUrl = empltyBookUrl + 1
 
         print("Empty Book Url Number :", empltyBookUrl)
         # Inserting into Excel File
         df = pd.DataFrame.from_dict(c, orient='index')
         df = df.transpose()
-        #df.to_excel(r'H:\Programming\pyCharm\WebScrappingPython\AllBook.xlsx', index=True, encoding='utf-8')
+        # df.to_excel(r'H:\Programming\pyCharm\WebScrappingPython\AllBook.xlsx', index=True, encoding='utf-8')
         df.to_excel(r'C:\Users\EATL\PycharmProjects\WebScapping\AllBook.xlsx', index=True, encoding='utf-8')
